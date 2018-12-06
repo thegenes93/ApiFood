9830cd69eb405d92cb225674c59b4b5ab14c7c05
Theogenes <41095513+thegenes93@users.noreply.github.com>
2018-12-06 09:54:40 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict'
 
-const app = require('../NoFood.Api/bin/express');
-const variables = require('../NoFood.Api/bin/configuration/variables');
+const app = require('../bin/express');
+const variables = require('../bin/configuration/variables');
 
 app.listen(variables.Api.port, () => {
     console.info(`Api inicializada com sucesso na porta ${variables.Api.port}`);
